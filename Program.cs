f881ae81766c6a2442be470b8ce4a597d6d74803
serzhSur <t1807999@yandex.ru>
2024-10-23 22:43:05 +0300

    create Handler

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-﻿
-using ConsoleSchedule.Models;
-using ConsoleSchedule.Services;
-using ConsoleSchedule.Repositories;
-using ConsoleSchedule;
+﻿using ConsoleSchedule.Services;
 
 string connectionString = "Host=localhost;Username=postgres;Password=Sur999; Database=schedule_test";
-Console.WriteLine("Start program...");
 try 
 { 
     //создание базы данных, таблиц, тестовых записей
@@ -13,45 +8,17 @@ try
     await dataBase.CreateDataBase("Host=localhost;Username=postgres;Password=Sur999", "schedule_test");
     await dataBase.CreateTables();
     await dataBase.CreateTestRecords(new DateTime(2024, 10, 14, 10, 0, 0));
-
-    //получение user
-    var userRopo = new UserRepository(connectionString);
-    User user4 = await userRopo.GetUserById(4);
-
-    //получение master
-    var masterRepo = new MasterRepository(connectionString);
-    Master master = await masterRepo.GetMasterById(2);
-
-    //получение списка сервисов(services) мастера и конкретного сервиса(service)
-    var serviceRepository = new ServiceRepository(connectionString);
-    var services = new List<Service>( await serviceRepository.GetMasterServices(master));
-    Service service = services.FirstOrDefault(s => s.Name=="hair-coloring");
-    // вывод услуг master
-    Console.WriteLine($"Master id: {master.Id}\tName: {master.Name}\tSpeciality: {master.Speciality}" +
-                      $"\tdayInterval: {master.Day_interval}");
-    foreach (var s in services)
-    {
-        Console.WriteLine($"service id: {s.Id}\tname: {s.Name}\tduration: {s.Duration}\tprice: 00");
-    }
     
-    //отмена записи к мастеру и создание другой записи
-    var appointmentRepo = new AppointmentRepository(connectionString);
-    var appointmentService = new AppointmentService(appointmentRepo);
-    await appointmentService.CancelAppointmentById(6);
-    if (service == null)
+    var handler = new InputOutputHandler(connectionString);
+    bool exit;
+    do
     {
-        throw new ArgumentException("Appointment's parametrs NOT Found", nameof(service));
+        await handler.Start();
+        Console.WriteLine("Для выхода введите: 2");
+        string input = Console.ReadLine();
+        exit = (input == "2");
     }
-    Appointment appointment = new Appointment(new DateTime(2024, 10, 14, 12, 30, 0), master, service, user4);
-    await appointmentService.MakeAppointment(appointment);
-
-    // вывод расписания для user
-    var appDetailRepo = new AppointmentDetailsRepository(connectionString);
-    var scheduleService = new ScheduleService(appointmentService, appDetailRepo);
-    scheduleService.ShowScheduleForUser(await scheduleService.CreateScheduleForUser(master));
-    
-    //Вывод расписания для Master
-    scheduleService.ShowScheduleDatail(await scheduleService.CreateScheduleForMaster(master));
+    while (!exit);
 }
 
 catch (Exception ex) 
